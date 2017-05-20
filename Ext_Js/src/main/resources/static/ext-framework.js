d9ccb39cc2739664ef322d42a7e5b8045af53112
Chen <fslichen863@gmail.com>
2017-05-20 21:50:51 +0800

    new demos

--- Ext_Js/src/main/resources/static/ext-framework.js
+++ Ext_Js/src/main/resources/static/ext-framework.js
@@ -1,4 +1,7 @@
-function fields(columnNames) {
+var panel;
+var grid;
+
+function getFields(columnNames) {
 	var fields = new Array();
 	fields.push({name : 'id', type : 'int'});
 	for (var i = 0; i < columnNames.length; i++) {
@@ -7,7 +10,8 @@ function fields(columnNames) {
 	return fields;
 }
 
-function rows(columnNames, responseJson) {
+// Extract columnNames from responseJson.
+function getRows(columnNames, responseJson) {
 	var rows = new Array();
 	responseJson = JSON.parse(responseJson);
 	for (var i = 0; i < responseJson.length; i++) {
@@ -21,7 +25,7 @@ function rows(columnNames, responseJson) {
 	return rows;
 }
 
-function columns(columnNames, columnAliases) {
+function getColumns(columnNames, columnAliases) {
 	columnAliases = columnAliases == null ? columnNames : columnAliases;
 	var columns = new Array();
 	columns.push({id : 'id', dataIndex: 'id', header : 'ID', width: 64});
@@ -31,27 +35,103 @@ function columns(columnNames, columnAliases) {
 	return columns;
 }
 
-function defaultTitle(title) {
-	return title == null ? 'Ext-Framework' : title;
-}
-
-function print(columnNames, responseJson, columnAliases, target, title) {
+// Collect columnNames within responseJson and print columnAliases under target.
+function setGrid(columnNames, responseJson, columnAliases, title) {
 	// Data
-	fields = fields(columnNames);
+	fields = getFields(columnNames);
 	var store = new Ext.data.SimpleStore({
 	    fields : fields
 	});
-	store.loadData(rows(columnNames, responseJson));
+	store.loadData(getRows(columnNames, responseJson));
 	// Grid
-	var viewSize = Ext.getBody().getViewSize();
-	var grid = new Ext.grid.GridPanel({
-	    store : store,
-	    columns : columns(columnNames, columnAliases),
-	    stripeRows : true,
-	    title : defaultTitle(title),
-	    width : viewSize.width,
-	    height : viewSize.height,
+	if (grid == null) {
+		grid = new Ext.grid.GridPanel({
+			store : store,
+			columns : getColumns(columnNames, columnAliases),
+			stripeRows : true,
+			title : title == null ? 'Ext-Framework' : title,
+			width : fullWidth(),
+			height : fullHeight(),
+		});
+	}
+}
+
+function add2Container(item) {
+	container.add(item);
+}
+
+function removeFromContainer(item) {
+	container.remove(item);
+}
+
+// Send requestJson to url, print columnNames or columnAliases under target.
+function post(requestJson, url, columnNames, columnAliases, title) {
+	Ext.Ajax.request({
+		url : url,// Example : /post
+		jsonData: requestJson,// Example : {name : 'Chen'}
+		success: function(responseJson) {
+			var responseJson = responseJson.responseText;
+			setGrid(columnNames, responseJson, columnAliases, title);
+			add2Container(grid);
+		},
+		failure: function() {
+			alert('Failure');
+		}
 	});
-	// Render
-	grid.render(target);        
+}
+
+function fullWidth() {
+	var viewSize = Ext.getBody().getViewSize();
+	return viewSize.width;
+}
+
+function fullHeight() {
+	var viewSize = Ext.getBody().getViewSize();
+	return viewSize.height;
+}
+
+function addPanel() {
+	var panel = Ext.create('Ext.panel.Panel', {
+	    width: fullWidth(),
+	    height: 100,
+	    title: 'Hello'
+	});
+	add2Container(panel);
+	return panel;
+}
+
+function addButton(buttonName, panel) {
+	var button = Ext.create('Ext.Button', {
+	    text : buttonName,
+	    handler : function() {
+	        alert('Did you just click the button?');
+	    }
+	});
+	panel.add(button);
+}
+
+function addTextField(textFieldName, textFieldAlias, panel) {
+	var textField = Ext.create('Ext.form.field.Text', {
+        fieldLabel: textFieldAlias,
+        name: textFieldName,
+        itemId: textFieldName,
+        autofocus: true,
+        enableKeyEvents: true,
+        labelAlign: 'right',
+        labelWidth: 64,
+        labelStyle: 'font-size: 16px;',
+        width: 256
+    });
+	panel.add(textField);
+}
+
+var container;
+
+function run(title) {
+	container = Ext.create('Ext.panel.Panel', {
+	    renderTo: Ext.getBody(),
+	    width: fullWidth(),
+	    height: fullHeight(),
+	    title: title
+	});
 }
